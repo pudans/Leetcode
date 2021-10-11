3d20fa39fc8e293105397a96c80cbbf27d20b6f0
kkarpyshev <kkarpyshev@ivi.ru>
2021-10-11 20:08:57 +0300

    543 daily challenge

--- src/easy/543_diameter_of_binary_tree.kt
+++ src/easy/543_diameter_of_binary_tree.kt
@@ -2,10 +2,18 @@ package easy
 
 import utils.TreeNode
 
+/**
+ * 543. Diameter of Binary Tree
+ * https://leetcode.com/problems/diameter-of-binary-tree/
+ *
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+ * The length of a path between two nodes is represented by the number of edges between them.
+ */
 
 class Solution534 {
 
-    var result = 1
+    var result = 0
 
     fun diameterOfBinaryTree(root: TreeNode?): Int {
         depth(root)
@@ -13,15 +21,10 @@ class Solution534 {
     }
 
     fun depth(root: TreeNode?): Int {
-        if (root == null) {
-            return 0
-        } else {
-            val leftDepth = depth(root.left)
-            val rightDepth = depth(root.right)
-
-            result = Math.max(result, leftDepth + rightDepth + 1)
-
-            return Math.max(rightDepth, leftDepth) + 1
-        }
+        if (root == null) return 0
+        val leftDepth = depth(root.left)
+        val rightDepth = depth(root.right)
+        result = Math.max(rightDepth + leftDepth + 1, result)
+        return Math.max(rightDepth, leftDepth) + 1
     }
 }
