2b7f203ada2c9648d100630a79cce5d56e06c1f1
kkarpyshev <kkarpyshev@ivi.ru>
2021-10-21 12:30:33 +0300

    Medium380 challenge

--- src/medium/380_insert_delete_getRandom_0(1).kt
+++ src/medium/380_insert_delete_getRandom_0(1).kt
@@ -2,33 +2,43 @@ package medium
 
 import kotlin.random.Random
 
-class RandomizedSet() {
+/**
+ * 380. Insert Delete GetRandom O(1)
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/
+ *
+ * Implement the RandomizedSet class:
+ * RandomizedSet() Initializes the RandomizedSet object.
+ * bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+ * bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+ * int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called).
+ * Each element must have the same probability of being returned.
+ * You must implement the functions of the class such that each function works in average O(1) time complexity.
+ */
 
-    private val map = HashMap<Int,Boolean>()
-    private val random = Random(System.currentTimeMillis())
+class Medium380 {
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    fun insert(`val`: Int): Boolean {
-        if (!map.getOrDefault(`val`, false)) {
-            map[`val`] = true
-            return true
-        } else {
-            return false
-        }
+    class RandomizedSet {
+
+        private val set = HashSet<Int>()
+        private val random = Random(System.nanoTime())
+
+        fun insert(`val`: Int): Boolean = set.add(`val`)
+
+        fun remove(`val`: Int): Boolean = set.remove(`val`)
+
+        fun getRandom(): Int = set.random(random)
     }
+}
 
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    fun remove(`val`: Int): Boolean {
-        return map.remove(`val`) != null
-    }
-
-    /** Get a random element from the set. */
-    fun getRandom(): Int {
-        if (map.size > 0) {
-            return map.keys.elementAt(random.nextInt(map.size))
-        } else {
-            return 0
-        }
-    }
-
+fun main() {
+    val set = Medium380.RandomizedSet()
+    println(set.insert(10))
+    println(set.insert(11))
+    println(set.insert(12))
+    println(set.insert(13))
+    println(set.remove(13))
+    println(set.getRandom())
+    println(set.getRandom())
+    println(set.getRandom())
+    println(set.getRandom())
 }
