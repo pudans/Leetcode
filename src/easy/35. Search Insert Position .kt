492a4fb5f1bb7c0cd4261248b63fd8c31d357d20
konstantin <kkarpyshev@ivi.ru>
2021-11-26 05:25:17 +0300

    Easy35 challange

--- src/easy/35. Search Insert Position .kt
+++ src/easy/35. Search Insert Position .kt
@@ -15,6 +15,24 @@ import BinarySearchTopic
 class Easy35: ArraysTopic, BinarySearchTopic {
 
     fun searchInsert(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.lastIndex
+        while (start <= end) {
+            if (start == end) return if (nums[start] >= target) start else start + 1
+            if (nums[start] == target) return start
+            if (nums[end] == target) return end
+            val mid = (start + end) / 2
+            if (nums[mid] == target) return mid
+            if (nums[mid] > target) {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        return if (nums[start] >= target) start else start + 1
+    }
+
+    fun searchInsert1(nums: IntArray, target: Int): Int {
         var result = 0
         for (i in nums.indices) {
             when {
@@ -33,4 +51,6 @@ fun main() {
     println(Easy35().searchInsert(nums = intArrayOf(1,3,5,6), target = 7))
     println(Easy35().searchInsert(nums = intArrayOf(1,3,5,6), target = 0))
     println(Easy35().searchInsert(nums = intArrayOf(1), target = 0))
+    println(Easy35().searchInsert(nums = intArrayOf(1), target = 1))
+    println(Easy35().searchInsert(nums = intArrayOf(3,5,7,9,10), target = 8))
 }
