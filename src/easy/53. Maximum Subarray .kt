d442a42bdf088b511b7188072d13ae7d27770cce
konstantin <kkarpyshev@ivi.ru>
2021-11-26 05:10:30 +0300

    Easy53 challange

--- src/easy/53. Maximum Subarray .kt
+++ src/easy/53. Maximum Subarray .kt
@@ -14,21 +14,20 @@ import DynamicProgrammingTopic
 class Easy53: ArraysTopic, DynamicProgrammingTopic {
 
     fun maxSubArray(nums: IntArray): Int {
-        var soFar = Int.MIN_VALUE
+        var result = Int.MIN_VALUE
         var currMax = 0
         nums.forEach {
-            if (currMax < 0) {
-                currMax = 0
-            }
-            soFar = Math.max(soFar, currMax + it)
+            if (currMax < 0) currMax = 0
             currMax += it
+            result = Math.max(result, currMax)
         }
-        return soFar
+        return result
     }
 }
 
 fun main() {
     println(Easy53().maxSubArray(intArrayOf(-2,1,-3,4,-1,2,1,-5,4)))
     println(Easy53().maxSubArray(intArrayOf(1)))
+    println(Easy53().maxSubArray(intArrayOf(-1)))
     println(Easy53().maxSubArray(intArrayOf(5,4,-1,7,8)))
 }
