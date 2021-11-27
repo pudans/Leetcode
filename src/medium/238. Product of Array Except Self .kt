80714a55e64a1e0321ce685867442e98cc34e56a
konstantin <kkarpyshev@ivi.ru>
2021-11-27 18:37:46 +0300

    Medium238 challange

--- src/medium/238. Product of Array Except Self .kt
+++ src/medium/238. Product of Array Except Self .kt
@@ -2,15 +2,15 @@ package medium
 
 import ArraysTopic
 
+
 /**
  * 238. Product of Array Except Self
  * https://leetcode.com/problems/product-of-array-except-self/
  *
  * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
-
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
 You must write an algorithm that runs in O(n) time and without using the division operation.
+ BULLSHIT
  */
 
 class Medium238 : ArraysTopic {
@@ -21,17 +21,15 @@ class Medium238 : ArraysTopic {
         for (i in 1 until nums.size) {
             result[i] = result[i - 1] * nums[i - 1]
         }
-
-        val cache = IntArray(nums.size)
-        cache[nums.size - 1] = 1
+        var right = nums.last()
         for (i in (nums.size - 2) downTo 0) {
-            cache[i] = cache[i + 1] * nums[i + 1]
+            result[i] = result[i] * right
+            right *= nums[i]
         }
-
-        for (i in nums.indices) {
-            result[i] = result[i] * cache[i]
-        }
-
         return result
     }
+}
+
+fun main() {
+    println(Medium238().productExceptSelf(intArrayOf(1,2,3,4)).toList())
 }
