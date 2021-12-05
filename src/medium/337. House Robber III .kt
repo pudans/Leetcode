dd56862a6ec158ea3cc9c8ae8f696d366297cd62
konstantin <kkarpyshev@ivi.ru>
2021-12-05 18:16:09 +0300

    Medium337 challenge

--- src/medium/337. House Robber III .kt
+++ src/medium/337. House Robber III .kt
@@ -6,6 +6,7 @@ import DynamicProgrammingTopic
 import TreeTopic
 import utils.TreeNode
 
+
 /**
  * 337. House Robber III
  * https://leetcode.com/problems/house-robber-iii/
@@ -18,15 +19,27 @@ Given the root of the binary tree, return the maximum amount of money the thief 
 
 class Medium337 : DynamicProgrammingTopic, TreeTopic, DFSTopic, BinaryTreeTopic {
 
+    val map = HashMap<TreeNode, Int>()
+
+    fun rob(root: TreeNode?): Int {
+        if (root == null) return 0
+        var max = 0
+        map[root]?.let { return it }
+        if (root.left != null) max += rob(root.left!!.left) + rob(root.left!!.right)
+        if (root.right != null) max += rob(root.right!!.left) + rob(root.right!!.right)
+        max = maxOf(max + root.`val`, rob(root.left) + rob(root.right))
+        map[root] = max
+        return max
+    }
+
     fun rob(root: TreeNode?, canRob: Boolean = true): Int {
         if (root == null) return 0
-        if (!canRob) {
-            return rob(root.right) + rob(root.left)
-        } else {
-            return Math.max(
-                    rob(root.right) + rob(root.left),
-                    root.`val` + rob(root.right, false) + rob(root.left, false)
+        return when (canRob) {
+            true -> maxOf(
+                rob(root.right) + rob(root.left),
+                root.`val` + rob(root.right, false) + rob(root.left, false)
             )
+            false -> rob(root.right) + rob(root.left)
         }
     }
 }
