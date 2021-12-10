c7b16b268e85fe3781d39e9dced5ba179dbcce74
kkarpyshev <kkarpyshev@ivi.ru>
2021-12-10 16:03:19 +0300

    Medium189 challenge

--- src/medium/189. Rotate Array .kt
+++ src/medium/189. Rotate Array .kt
@@ -13,10 +13,17 @@ import TwoPointersTopic
 
 class Medium189 : ArraysTopic, TwoPointersTopic, MathTopic {
 
-    fun rotate(nums: IntArray, k: Int): Unit {
+    fun rotate(nums: IntArray, k: Int) {
         val clone = nums.clone()
         for (i in nums.indices) {
             nums[(i + k) % nums.size] = clone[i]
         }
     }
+}
+
+fun main() {
+    val array = intArrayOf(1,2,3,4,5,6,7,8,9)
+    println(array.toList())
+    Medium189().rotate(array, 3)
+    println(array.toList())
 }
