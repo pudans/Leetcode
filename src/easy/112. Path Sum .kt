2095cae8d35514e0e4bcba1a2c20f5f79138c708
konstantin <kkarpyshev@ivi.ru>
2022-01-03 21:24:53 +0300

    Easy112 challenge

--- src/easy/112. Path Sum .kt
+++ src/easy/112. Path Sum .kt
@@ -13,19 +13,14 @@ import utils.TreeNode
 
 class Easy112 {
 
-    fun hasPathSum(root: TreeNode?, sum: Int): Boolean {
-        if (root == null) return false
-        return isPathSum(root, sum)
-    }
-
-    private fun isPathSum(root: TreeNode, targetSum: Int, currentSum: Int = 0): Boolean {
-        val newSum = currentSum + root.`val`
-        return when {
-            root.left == null && root.right == null -> newSum == targetSum
+    fun hasPathSum(root: TreeNode?, targetSum: Int, currentSum: Int = 0): Boolean =
+        when {
+            root == null -> false
+            root.left == null && root.right == null -> currentSum + root.`val` == targetSum
             root.left != null && root.right != null ->
-                isPathSum(root.right!!, targetSum, newSum) || isPathSum(root.left!!, targetSum, newSum)
-            root.left != null -> isPathSum(root.left!!, targetSum, newSum)
-            else -> isPathSum(root.right!!, targetSum, newSum)
+                hasPathSum(root.right!!, targetSum, currentSum + root.`val`)
+                        || hasPathSum(root.left!!, targetSum, currentSum + root.`val`)
+            root.left != null -> hasPathSum(root.left!!, targetSum, currentSum + root.`val`)
+            else -> hasPathSum(root.right!!, targetSum, currentSum + root.`val`)
         }
-    }
 }
