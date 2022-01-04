92a0c537b6a80f67debc46097287fb4899d9591f
konstantin <kkarpyshev@ivi.ru>
2022-01-04 03:19:40 +0300

    Easy701 challenge

---
+++ src/easy/701. Insert into a Binary Search Tree .kt
@@ -0,0 +1,36 @@
+package easy
+
+import BinarySearchTopic
+import BinaryTreeTopic
+import TwoPointersTopic
+import utils.TreeNode
+
+/**
+ * 701. Insert into a Binary Search Tree
+ * https://leetcode.com/problems/insert-into-a-binary-search-tree/
+ *
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion.
+It is guaranteed that the new value does not exist in the original BST.
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+ */
+
+class Easy701 : BinaryTreeTopic, BinarySearchTopic {
+
+    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+        root ?: return TreeNode(`val`)
+        if (`val` > root.`val`) {
+            if (root.right == null) {
+                root.right = TreeNode(`val`)
+            } else {
+                insertIntoBST(root.right, `val`)
+            }
+        } else {
+            if (root.left == null) {
+                root.left = TreeNode(`val`)
+            } else {
+                insertIntoBST(root.left, `val`)
+            }
+        }
+        return root
+    }
+}
