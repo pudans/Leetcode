340994e9ef1b1ed48cdb1f6b3b0c98d785ef3fc6
konstantin <kkarpyshev@ivi.ru>
2022-01-09 00:52:33 +0300

    Medium19 challenge

--- src/medium/19. Remove Nth Node From End of List .kt
+++ src/medium/19. Remove Nth Node From End of List .kt
@@ -3,6 +3,8 @@ package medium
 import LinkedListTopic
 import TwoPointersTopic
 import utils.ListNode
+import utils.add
+import utils.toStringList
 
 /**
  * 19. Remove Nth Node From End of List
@@ -11,21 +13,29 @@ import utils.ListNode
  * Given the head of a linked list, remove the nth node from the end of the list and return its head.
  */
 
-class Medium19: LinkedListTopic, TwoPointersTopic {
+class Medium19 : LinkedListTopic, TwoPointersTopic {
 
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-        val list = ArrayList<ListNode>()
+        head ?: return null
+        var dd = head
         var temp = head
+        var i = 0
         while (temp != null) {
-            list.add(temp)
+            if (i > n) {
+                dd = dd?.next
+            }
             temp = temp.next
+            i++
         }
-        return if (n == list.size) {
-            head?.next
-        } else {
-            val dd = list[list.size - (n + 1)]
-            dd.next = dd.next?.next
-            head
-        }
+        if (i < n) return null
+        if (i == n) return head.next
+        dd?.next = dd?.next?.next
+        return head
     }
+}
+
+fun main() {
+    val head = ListNode(1)
+    head.add(ListNode(2)).add(ListNode(3)).add(ListNode(4)).add(ListNode(5))
+    println(Medium19().removeNthFromEnd(head, 2)?.toStringList())
 }
