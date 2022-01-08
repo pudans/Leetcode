8de8c09d2f92c3f08f82189d89fe7fcf451341ce
konstantin <kkarpyshev@ivi.ru>
2022-01-09 00:42:11 +0300

    Easy876 challenge

--- src/easy/876. Middle of the Linked List .kt
+++ src/easy/876. Middle of the Linked List .kt
@@ -1,6 +1,9 @@
 package easy
 
+import LinkedListTopic
+import TwoPointersTopic
 import utils.ListNode
+import utils.add
 
 /**
  * 876. Middle of the Linked List
@@ -10,19 +13,25 @@ import utils.ListNode
  * If there are two middle nodes, return the second middle node.
  */
 
-class Easy876 {
+class Easy876 : LinkedListTopic, TwoPointersTopic {
 
     fun middleNode(head: ListNode?): ListNode? {
         var temp = head
         var result = head
         var i = 0
         while (temp != null) {
-            temp = temp.next
-            if (i % 2 == 0 && temp != null) {
+            if (i % 2 == 1) {
                 result = result?.next
             }
+            temp = temp.next
             i++
         }
         return result
     }
+}
+
+fun main() {
+    val head = ListNode(1)
+    head.add(ListNode(2)).add(ListNode(3)).add(ListNode(4)).add(ListNode(5))
+    println(Easy876().middleNode(head)?.`val`)
 }
