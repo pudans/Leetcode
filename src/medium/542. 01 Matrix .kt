ca5f351a759e2d1691c9ecff1b88f9ab3d843dd9
konstantin <kkarpyshev@ivi.ru>
2022-01-12 13:55:46 +0300

    Medium542 challenge

--- src/medium/542. 01 Matrix .kt
+++ src/medium/542. 01 Matrix .kt
@@ -14,29 +14,25 @@ The distance between two adjacent cells is 1.
 
 class Medium542 : MatrixTopic, BFSTopic, DynamicProgrammingTopic {
 
-    fun updateMatrix(mat: Array<IntArray>): Array<IntArray> {
-        val min = Array(mat.size) { IntArray(mat.first().size) { Int.MAX_VALUE - 10000 } }
-
-        for (i in mat.indices) {
-            for (j in mat.first().indices) {
-                if (mat[i][j] == 0) {
-                    min[i][j] = 0
+    fun updateMatrix(matrix: Array<IntArray>): Array<IntArray> {
+        val result = Array(matrix.size) { IntArray(matrix.first().size) { Int.MAX_VALUE - 10000 } }
+        for (i in matrix.indices) {
+            for (j in matrix.first().indices) {
+                if (matrix[i][j] == 0) {
+                    result[i][j] = 0
                     continue
                 }
-                if (i > 0) min[i][j] = minOf(min[i][j], min[i-1][j] + 1)
-                if (j > 0) min[i][j] = minOf(min[i][j], min[i][j-1] + 1)
+                if (i > 0) result[i][j] = minOf(result[i][j], result[i - 1][j] + 1)
+                if (j > 0) result[i][j] = minOf(result[i][j], result[i][j - 1] + 1)
             }
         }
-        for (i in mat.lastIndex downTo 0) {
-            for (j in mat.first().lastIndex downTo 0) {
-                if (i < mat.lastIndex) min[i][j] = minOf(min[i][j], min[i+1][j] + 1)
-                if (j < mat.first().lastIndex) min[i][j] = minOf(min[i][j], min[i][j+1] + 1)
+        for (i in matrix.lastIndex downTo 0) {
+            for (j in matrix.first().lastIndex downTo 0) {
+                if (i < matrix.lastIndex) result[i][j] = minOf(result[i][j], result[i + 1][j] + 1)
+                if (j < matrix.first().lastIndex) result[i][j] = minOf(result[i][j], result[i][j + 1] + 1)
             }
         }
-
-        println(min.map { it.toList() }.map { it.toList() })
-
-        return min
+        return result
     }
 }
 
