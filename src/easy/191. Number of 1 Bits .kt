4529fea19d3b36682d160b4ee8556f064c738bf3
konstantin <kkarpyshev@ivi.ru>
2022-01-16 12:03:18 +0300

    Easy191 challenge

--- src/easy/191. Number of 1 Bits .kt
+++ src/easy/191. Number of 1 Bits .kt
@@ -1,14 +1,17 @@
 package easy
 
+import BitManipulationTopic
+
 /**
  * 191. Number of 1 Bits
  * https://leetcode.com/problems/number-of-1-bits/
  *
  * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+ * BULLSHIT
  */
 
-class Easy191 {
-//     you need to treat n as an unsigned value
+class Easy191 : BitManipulationTopic {
+
     fun hammingWeight(n: Int): Int {
         var input: Long = if (n < 0) 2L * Int.MAX_VALUE + 2L + n else n.toLong()
         var result = 0
