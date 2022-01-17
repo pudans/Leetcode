cfff7556cfdf27f5f99f5a8bd37f0f743a03fc26
konstantin <kkarpyshev@ivi.ru>
2022-01-17 11:48:57 +0300

    Medium74 challenge

--- src/medium/74. Search a 2D Matrix .kt
+++ src/medium/74. Search a 2D Matrix .kt
@@ -2,7 +2,6 @@ package medium
 
 import ArraysTopic
 import BinarySearchTopic
-import HashTableTopic
 import MatrixTopic
 
 /**
@@ -14,18 +13,16 @@ Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
  */
 
-class Medium74: ArraysTopic, BinarySearchTopic, MatrixTopic {
+class Medium74 : ArraysTopic, BinarySearchTopic, MatrixTopic {
 
     fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
         val r = matrix.size
         val c = matrix.first().size
         var start = 0
         var end = r * c - 1
-
         while (start <= end) {
             val mid = (start + end) / 2
             val res = matrix[mid / c][mid % c]
-//            println("$start $end $mid $res")
             if (res == target) return true
             if (res > target) {
                 end = mid - 1
@@ -38,7 +35,23 @@ class Medium74: ArraysTopic, BinarySearchTopic, MatrixTopic {
 }
 
 fun main() {
-    println(Medium74().searchMatrix(arrayOf(intArrayOf(1,3,5,7), intArrayOf(10,11,16,20), intArrayOf(23,30,34,60)), 3))
-    println(Medium74().searchMatrix(arrayOf(intArrayOf(1,3,5,7), intArrayOf(10,11,16,20), intArrayOf(23,30,34,60)), 13))
-    println(Medium74().searchMatrix(arrayOf(intArrayOf(1,3)), 3))
+    println(
+        Medium74().searchMatrix(
+            arrayOf(
+                intArrayOf(1, 3, 5, 7),
+                intArrayOf(10, 11, 16, 20),
+                intArrayOf(23, 30, 34, 60)
+            ), 3
+        )
+    )
+    println(
+        Medium74().searchMatrix(
+            arrayOf(
+                intArrayOf(1, 3, 5, 7),
+                intArrayOf(10, 11, 16, 20),
+                intArrayOf(23, 30, 34, 60)
+            ), 13
+        )
+    )
+    println(Medium74().searchMatrix(arrayOf(intArrayOf(1, 3)), 3))
 }
