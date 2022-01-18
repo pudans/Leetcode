a6a24df269ab162ad1e2e2af3950aef6bab414e5
konstantin <kkarpyshev@ivi.ru>
2022-01-18 12:07:48 +0300

    Medium153 challenge

--- src/medium/153. Find Minimum in Rotated Sorted Array .kt
+++ src/medium/153. Find Minimum in Rotated Sorted Array .kt
@@ -16,26 +16,24 @@ import BinarySearchTopic
  * You must write an algorithm that runs in O(log n) time.
  */
 
-class Medium153: BinarySearchTopic {
+class Medium153 : BinarySearchTopic {
 
     fun findMin(nums: IntArray): Int {
         if (nums.isEmpty()) return -1
         if (nums.size == 1) return nums.first()
-
-        var fromIndex = 0
-        var toIndex = nums.size - 1
-
-        while (fromIndex < toIndex) {
-            if (nums[fromIndex] < nums[toIndex]) return nums[fromIndex]
-            val mid = fromIndex + (toIndex - fromIndex) / 2
-            if (nums[mid] < nums[fromIndex]) toIndex = mid else fromIndex = mid + 1
+        var start = 0
+        var end = nums.lastIndex
+        while (start < end) {
+            if (nums[start] < nums[end]) return nums[start]
+            val mid = start + (end - start) / 2
+            if (nums[mid] < nums[start]) end = mid else start = mid + 1
         }
-        return nums[fromIndex]
+        return nums[start]
     }
 }
 
 fun main() {
-    println(Medium153().findMin(intArrayOf(3,4,5,1,2)))
-    println(Medium153().findMin(intArrayOf(4,5,6,7,0,1,2)))
-    println(Medium153().findMin(intArrayOf(11,13,15,17)))
+    println(Medium153().findMin(intArrayOf(3, 4, 5, 1, 2)))
+    println(Medium153().findMin(intArrayOf(4, 5, 6, 7, 0, 1, 2)))
+    println(Medium153().findMin(intArrayOf(11, 13, 15, 17)))
 }
