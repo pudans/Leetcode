70b8d7b1586cffa582ce6af5a66d29975ec14a90
konstantin <kkarpyshev@ivi.ru>
2022-01-18 13:25:39 +0300

    Medium24 challenge

---
+++ src/medium/24. Swap Nodes in Pairs .kt
@@ -0,0 +1,33 @@
+package medium
+
+import LinkedListTopic
+import utils.ListNode
+
+/**
+ * 24. Swap Nodes in Pairs
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
+ *
+Given a linked list, swap every two adjacent nodes and return its head.
+You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+ */
+
+class Medium24 : LinkedListTopic {
+
+    fun swapPairs(head: ListNode?): ListNode? {
+        head ?: return null
+        head.next ?: return head
+        val sentinel = ListNode(0)
+        sentinel.next = head
+        var step = sentinel
+        while (true) {
+            val temp1 = step.next
+            val temp2 = step.next?.next
+            if (temp1 == null || temp2 == null) break
+            step.next = temp2
+            temp1.next = temp2.next
+            temp2.next = temp1
+            step = temp1
+        }
+        return sentinel.next
+    }
+}
