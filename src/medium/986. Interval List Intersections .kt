e6d9a8690cedead2e1a78cfe59a4a66c6ea271ba
konstantin <kkarpyshev@ivi.ru>
2022-01-20 21:00:36 +0300

    Medium986 challenge

--- src/medium/986. Interval List Intersections .kt
+++ src/medium/986. Interval List Intersections .kt
@@ -16,21 +16,16 @@ The intersection of two closed intervals is a set of real numbers that are eithe
 For example, the intersection of [1, 3] and [2, 4] is [2, 3].
  */
 
-class Medium986: ArraysTopic, TwoPointersTopic {
+class Medium986 : ArraysTopic, TwoPointersTopic {
 
     fun intervalIntersection(A: Array<IntArray>, B: Array<IntArray>): Array<IntArray> {
-        val ans: MutableList<IntArray> = ArrayList()
+        val ans = mutableListOf<IntArray>()
         var i = 0
         var j = 0
         while (i < A.size && j < B.size) {
-            // Let's check if A[i] intersects B[j].
-            // lo - the startpoint of the intersection
-            // hi - the endpoint of the intersection
-            val lo = Math.max(A[i][0], B[j][0])
-            val hi = Math.min(A[i][1], B[j][1])
+            val lo = maxOf(A[i][0], B[j][0])
+            val hi = minOf(A[i][1], B[j][1])
             if (lo <= hi) ans.add(intArrayOf(lo, hi))
-
-            // Remove the interval with the smallest endpoint
             if (A[i][1] < B[j][1]) i++ else j++
         }
         return ans.toTypedArray()
@@ -40,7 +35,7 @@ class Medium986: ArraysTopic, TwoPointersTopic {
 
 fun main() {
     println(Medium986().intervalIntersection(
-            arrayOf(intArrayOf(0,2), intArrayOf(5,10), intArrayOf(13,23), intArrayOf(24,25)),
-            arrayOf(intArrayOf(1,5), intArrayOf(8,12), intArrayOf(15,24), intArrayOf(25,26))
-            ).toList().map { it.toList() })
+        arrayOf(intArrayOf(0, 2), intArrayOf(5, 10), intArrayOf(13, 23), intArrayOf(24, 25)),
+        arrayOf(intArrayOf(1, 5), intArrayOf(8, 12), intArrayOf(15, 24), intArrayOf(25, 26))
+    ).toList().map { it.toList() })
 }
