2878c7e2c78a1c095e24b8d4dea2c07b63a601ec
konstantin <kkarpyshev@ivi.ru>
2022-01-20 21:14:24 +0300

    Easy844 challenge

--- src/easy/844. Backspace String Compare .kt
+++ src/easy/844. Backspace String Compare .kt
@@ -1,5 +1,10 @@
 package easy
 
+import StackTopic
+import StringTopic
+import TwoPointersTopic
+import java.util.*
+
 /**
  * 844. Backspace String Compare
  * https://leetcode.com/problems/backspace-string-compare/
@@ -9,9 +14,23 @@ package easy
  * Note that after backspacing an empty text, the text will continue empty.
  */
 
-class Easy844 {
+class Easy844 : TwoPointersTopic, StackTopic, StringTopic {
 
-    fun backspaceCompare(S: String, T: String): Boolean {
+    fun backspaceCompare(S: String, T: String): Boolean = build(S) == build(T)
+
+    private fun build(str: String): String {
+        val stack = Stack<Char>()
+        str.forEach { ch ->
+            if (ch == '#') {
+                if (stack.size > 0) stack.pop()
+            } else {
+                stack.push(ch)
+            }
+        }
+        return stack.joinToString("")
+    }
+
+    fun backspaceCompare1(S: String, T: String): Boolean {
 
         var i = 0
         var j = 0
@@ -24,10 +43,10 @@ class Easy844 {
             if (i < strA.length) {
                 if (strA[i] == '#') {
                     if (i != 0) {
-                        strA = strA.replaceRange(IntRange(i-1,i),"")
+                        strA = strA.replaceRange(IntRange(i - 1, i), "")
                         i--
                     } else {
-                        strA = strA.replaceRange(IntRange(i,i),"")
+                        strA = strA.replaceRange(IntRange(i, i), "")
                     }
                 } else {
                     i++
@@ -37,10 +56,10 @@ class Easy844 {
             if (j < strB.length) {
                 if (strB[j] == '#') {
                     if (j != 0) {
-                        strB = strB.replaceRange(IntRange(j-1,j),"")
+                        strB = strB.replaceRange(IntRange(j - 1, j), "")
                         j--
                     } else {
-                        strB = strB.replaceRange(IntRange(j,j),"")
+                        strB = strB.replaceRange(IntRange(j, j), "")
                     }
                 } else {
                     j++
