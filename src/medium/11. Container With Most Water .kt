fc634bd92f3b8d59792b021119fb007b05fd9781
konstantin <kkarpyshev@ivi.ru>
2022-01-20 21:48:21 +0300

    Medium11 challenge

--- src/medium/11. Container With Most Water .kt
+++ src/medium/11. Container With Most Water .kt
@@ -4,6 +4,7 @@ import ArraysTopic
 import GreedyTopic
 import TwoPointersTopic
 
+
 /**
  * 11. Container With Most Water
  * https://leetcode.com/problems/container-with-most-water/
@@ -14,13 +15,31 @@ import TwoPointersTopic
  * Notice that you may not slant the container.
  */
 
-class Medium11: ArraysTopic, TwoPointersTopic, GreedyTopic {
+class Medium11 : ArraysTopic, TwoPointersTopic, GreedyTopic {
 
-    fun maxArea(height: IntArray): Int {
+    fun maxArea(height: IntArray?): Int {
+        if (height == null || height.isEmpty()) return 0
+        var start = 0
+        var end = height.lastIndex
         var result = 0
-        for (i in 0 until height.size - 1) {
+        while (start < end) {
+            val volume = (end - start) * minOf(height[start], height[end])
+            result = maxOf(result, volume)
+            if (height[start] < height[end]) {
+                start++
+            } else {
+                end--
+            }
+        }
+        return result
+    }
+
+    // mine
+    fun maxArea1(height: IntArray): Int {
+        var result = 0
+        for (i in 0 until height.lastIndex) {
             for (j in i + 1 until height.size) {
-                val res = Math.min(height[i],height[j]) * (j - i)
+                val res = minOf(height[i], height[j]) * (j - i)
                 if (res > result) {
                     result = res
                 }
@@ -31,7 +50,7 @@ class Medium11: ArraysTopic, TwoPointersTopic, GreedyTopic {
 }
 
 fun main() {
-    println(Medium11().maxArea(intArrayOf(1,1)))
-    println(Medium11().maxArea(intArrayOf(4,3,2,1,4)))
-    println(Medium11().maxArea(intArrayOf(1,2,1)))
+    println(Medium11().maxArea(intArrayOf(1, 1)))
+    println(Medium11().maxArea(intArrayOf(4, 3, 2, 1, 4)))
+    println(Medium11().maxArea(intArrayOf(1, 2, 1)))
 }
