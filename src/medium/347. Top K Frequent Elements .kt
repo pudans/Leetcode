f9db48763e7cc61581d627a63a02bfc5d1fae298
konstantin <kkarpyshev@ivi.ru>
2022-01-25 21:25:42 +0300

    Medium347 challenge

--- src/medium/347. Top K Frequent Elements .kt
+++ src/medium/347. Top K Frequent Elements .kt
@@ -15,24 +15,21 @@ import java.util.*
 class Medium347 : HashTableTopic, SortingTopic, HeapTopic {
 
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
-
         val map = HashMap<Int, Int>()
-
         nums.forEach {
             val value = map.getOrDefault(it, 0) + 1
-            map.put(it, value)
+            map[it] = value
         }
-
-        val heap = PriorityQueue<Int>() { o1: Int, o2: Int -> (map[o1] ?: 0) - (map[o2] ?: 0) }
-
+        val heap = PriorityQueue { o1: Int, o2: Int -> (map[o1] ?: 0) - (map[o2] ?: 0) }
         map.keys.forEach { n ->
             heap.add(n)
             if (heap.size > k) heap.poll()
         }
-
-        val result = IntArray(k) {
-            heap.poll()
-        }
-        return result
+        return heap.toIntArray()
     }
+}
+
+fun main() {
+    println(Medium347().topKFrequent(intArrayOf(1, 1, 1, 2, 2, 3), 2).toList())
+    println(Medium347().topKFrequent(intArrayOf(1), 1).toList())
 }
