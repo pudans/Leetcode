4bc698dffc0b8d2f8f0f8b831e08bd9e84e6fba2
konstantin <kkarpyshev@ivi.ru>
2022-01-26 13:15:00 +0300

    Medium973 challenge

--- src/medium/973. K Closest Points to Origin .kt
+++ src/medium/973. K Closest Points to Origin .kt
@@ -1,5 +1,6 @@
 package medium
 
+import MathTopic
 import SortingTopic
 import java.util.*
 
@@ -13,16 +14,18 @@ The distance between two points on the X-Y plane is the Euclidean distance (i.e.
 You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
  */
 
-class Medium973 : SortingTopic {
+class Medium973 : SortingTopic, MathTopic {
 
     fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
         fun IntArray.len() = this[0] * this[0] + this[1] * this[1]
-        points.sortWith(kotlin.Comparator() { o1, o2 -> o1.len() - o2.len() } )
+        points.sortWith(kotlin.Comparator() { o1, o2 -> o1.len() - o2.len() })
         return Array(k) { points[it] }
     }
 }
 
 fun main() {
-    println(Medium973().kClosest(arrayOf(intArrayOf(1,3), intArrayOf(-2,2)), 1).toList().map { it.toList() })
-    println(Medium973().kClosest(arrayOf(intArrayOf(3,3), intArrayOf(5,-1), intArrayOf(-2,4)), 2).toList().map { it.toList() })
+    println(Medium973().kClosest(arrayOf(intArrayOf(1, 3), intArrayOf(-2, 2)), 1).toList().map { it.toList() })
+    println(
+        Medium973().kClosest(arrayOf(intArrayOf(3, 3), intArrayOf(5, -1), intArrayOf(-2, 4)), 2).toList()
+            .map { it.toList() })
 }
