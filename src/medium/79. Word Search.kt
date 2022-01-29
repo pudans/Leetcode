5cf0027fdb2760f79003705f86fa3610b523dec8
konstantin <kkarpyshev@ivi.ru>
2022-01-29 17:45:13 +0300

    Medium79 challenge

--- src/medium/79. Word Search.kt
+++ src/medium/79. Word Search.kt
@@ -11,9 +11,10 @@ import MatrixTopic
  * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
  * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.
  * The same letter cell may not be used more than once.
+ * BULLSHIT
  */
 
-class Medium79: ArraysTopic, BacktrackingTopic, MatrixTopic {
+class Medium79 : ArraysTopic, BacktrackingTopic, MatrixTopic {
 
     lateinit var board: Array<CharArray>
     lateinit var word: String
@@ -46,22 +47,22 @@ class Medium79: ArraysTopic, BacktrackingTopic, MatrixTopic {
 //        println("$i $j $index $isLast ${set.toString()}")
 
         return when {
-            isLast && i != 0 && isTrue(i-1,j,index, set) -> true
-            isLast && j != 0 && isTrue(i,j-1,index, set) -> true
-            isLast && i+1 < board.size && isTrue(i+1,j,index, set) -> true
-            isLast && j+1 < board[0].size && isTrue(i,j+1,index, set) -> true
+            isLast && i != 0 && isTrue(i - 1, j, index, set) -> true
+            isLast && j != 0 && isTrue(i, j - 1, index, set) -> true
+            isLast && i + 1 < board.size && isTrue(i + 1, j, index, set) -> true
+            isLast && j + 1 < board[0].size && isTrue(i, j + 1, index, set) -> true
 
             else -> {
-                if (i != 0 && isTrue(i-1,j,index, set) && find(i-1,j,index+1, HashSet(set))) {
+                if (i != 0 && isTrue(i - 1, j, index, set) && find(i - 1, j, index + 1, HashSet(set))) {
                     return true
                 }
-                if (j != 0 && isTrue(i,j-1,index, set) && find(i,j-1,index+1, HashSet(set))) {
+                if (j != 0 && isTrue(i, j - 1, index, set) && find(i, j - 1, index + 1, HashSet(set))) {
                     return true
                 }
-                if (i+1 < board.size && isTrue(i+1,j,index, set) && find(i+1,j,index+1, HashSet(set))) {
+                if (i + 1 < board.size && isTrue(i + 1, j, index, set) && find(i + 1, j, index + 1, HashSet(set))) {
                     return true
                 }
-                if (j+1 < board[0].size && isTrue(i,j+1,index, set) && find(i,j+1,index+1, HashSet(set))) {
+                if (j + 1 < board[0].size && isTrue(i, j + 1, index, set) && find(i, j + 1, index + 1, HashSet(set))) {
                     return true
                 }
                 return false
@@ -70,5 +71,5 @@ class Medium79: ArraysTopic, BacktrackingTopic, MatrixTopic {
     }
 
     fun isTrue(i: Int, j: Int, index: Int, set: HashSet<Int>): Boolean =
-            !set.contains(i * 1000 + j) && board[i][j] == word[index]
+        !set.contains(i * 1000 + j) && board[i][j] == word[index]
 }
