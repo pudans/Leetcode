1b3b7badea2bd6afb58a69fac3665e142ef87f0d
konstantin <kkarpyshev@ivi.ru>
2022-01-29 16:50:36 +0300

    Hard84 challenge

--- src/hard/84. Largest Rectangle in Histogram .kt
+++ src/hard/84. Largest Rectangle in Histogram .kt
@@ -4,7 +4,6 @@ import ArraysTopic
 import StackTopic
 import java.util.*
 
-
 /**
  * 84. Largest Rectangle in Histogram
  * https://leetcode.com/problems/largest-rectangle-in-histogram/
@@ -15,35 +14,16 @@ import java.util.*
 
 class Hard84 : ArraysTopic, StackTopic {
 
-//    fun largestRectangleArea(heights: IntArray): Int {
-//        val set = heights.toHashSet()
-//        var result = 0
-//        set.forEach { i ->
-//            var currMaxLen = 0
-//            heights.forEach {
-//                if (it < i) {
-//                    result = Math.max(result, currMaxLen * i)
-//                    currMaxLen = 0
-//                }
-//                else currMaxLen++
-//            }
-//            if (currMaxLen > 0) {
-//                result = Math.max(result, currMaxLen * i)
-//            }
-//        }
-//        return result
-//    }
-
     fun largestRectangleArea(heights: IntArray): Int {
         val stack = Stack<Int>()
         var max = 0
         stack.push(-1)
         for (i in 0..heights.size) {
-            val value = heights.getOrElse(i) { 0 }
+            val value = heights.getOrNull(i) ?: 0
             while (stack.peek() >= 0 && heights[stack.peek()] >= value) {
                 val height = heights[stack.pop()]
                 val start = stack.peek()
-                max = Math.max(max, height * (i - start - 1))
+                max = maxOf(max, height * (i - start - 1))
             }
             stack.push(i)
         }
@@ -52,6 +32,6 @@ class Hard84 : ArraysTopic, StackTopic {
 }
 
 fun main() {
-    println(Hard84().largestRectangleArea(intArrayOf(2,1,5,6,2,3)))
-    println(Hard84().largestRectangleArea(intArrayOf(2,4)))
+    println(Hard84().largestRectangleArea(intArrayOf(2, 1, 5, 6, 2, 3)))
+    println(Hard84().largestRectangleArea(intArrayOf(2, 4)))
 }
