8693b8779ace19787d49d17293925c9920161caa
konstantin <kkarpyshev@ivi.ru>
2022-01-29 17:03:18 +0300

    Medium200 challenge

--- src/medium/200. Number of Islands .kt
+++ src/medium/200. Number of Islands .kt
@@ -16,61 +16,38 @@ import MatrixTopic
 
 class Medium200 : ArraysTopic, MatrixTopic, DFSTopic, BFSTopic {
 
-    lateinit var visited: Array<BooleanArray>
-    lateinit var grid: Array<CharArray>
-
     fun numIslands(grid: Array<CharArray>): Int {
         var result = 0
-        this.grid = grid
-        this.visited = Array(grid.size) {
-            BooleanArray(grid[0].size) { false }
-        }
-
+        val visited = Array(grid.size) { BooleanArray(grid.first().size) }
         for (i in grid.indices) {
             for (j in grid[i].indices) {
-                if (grid[i][j] == '0') {
-                    continue
+                if (findLand(i, j, grid, visited)) {
+                    result++
                 }
-                if (visited[i][j]) {
-                    continue
-                }
-                findLand(i, j)
-                println("-----------")
-                result++
             }
         }
         return result
     }
 
-    private fun findLand(i: Int, j: Int) {
-        println("$i-$j")
-        if (grid[i][j] == '0') {
-            return
-        }
-        if (visited[i][j]) {
-            return
-        }
+    private fun findLand(i: Int, j: Int, grid: Array<CharArray>, visited: Array<BooleanArray>): Boolean {
+        if (i !in grid.indices) return false
+        if (j !in grid.first().indices) return false
+        if (grid[i][j] == '0') return false
+        if (visited[i][j]) return false
         visited[i][j] = true
-        if (i < grid.size - 1) {
-            findLand(i + 1, j)
-        }
-        if (j < grid[i].size - 1) {
-            findLand(i, j + 1)
-        }
-        if (j > 0) {
-            findLand(i, j - 1)
-        }
-        if (i > 0) {
-            findLand(i - 1, j)
-        }
+        findLand(i + 1, j, grid, visited)
+        findLand(i, j + 1, grid, visited)
+        findLand(i, j - 1, grid, visited)
+        findLand(i - 1, j, grid, visited)
+        return true
     }
 }
 
 fun main() {
     val grid = arrayOf(
-            charArrayOf('1', '0', '1', '1', '1'),
-            charArrayOf('1', '0', '1', '0', '1'),
-            charArrayOf('1', '1', '1', '0', '1')
+        charArrayOf('1', '0', '1', '1', '1'),
+        charArrayOf('1', '0', '1', '0', '1'),
+        charArrayOf('1', '1', '1', '0', '1')
     )
     println(Medium200().numIslands(grid))
 }
