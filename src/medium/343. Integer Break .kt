1bcfac95a5844329ae1ff6ba51bc009313ad434b
konstantin <kkarpyshev@ivi.ru>
2022-02-03 13:00:37 +0300

    Medium343 challenge

--- src/medium/343. Integer Break .kt
+++ src/medium/343. Integer Break .kt
@@ -9,6 +9,8 @@ import MathTopic
  *
 Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
 Return the maximum product you can get.
+
+ BULLSHIT
  */
 
 class Medium343 : DynamicProgrammingTopic, MathTopic {
