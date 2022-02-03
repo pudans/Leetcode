48ad15ac8189a041300cf7e3495a825d5c7cb764
konstantin <kkarpyshev@ivi.ru>
2022-02-03 12:49:03 +0300

    Medium322 challenge

--- src/medium/322. Coin Change .kt
+++ src/medium/322. Coin Change .kt
@@ -7,10 +7,9 @@ import DynamicProgrammingTopic
  * 322. Coin Change
  * https://leetcode.com/problems/coin-change/
  *
- * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-
-Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
-
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
 You may assume that you have an infinite number of each kind of coin.
  */
 
@@ -22,7 +21,7 @@ class Medium322 : DynamicProgrammingTopic, BFSTopic {
         for (i in 1 until dd.size) {
             for (coin in coins) {
                 if (coin <= i && dd[i - coin] != Int.MAX_VALUE) {
-                    dd[i] = Math.min(dd[i - coin] + 1, dd[i])
+                    dd[i] = minOf(dd[i - coin] + 1, dd[i])
                 }
             }
         }
@@ -32,4 +31,6 @@ class Medium322 : DynamicProgrammingTopic, BFSTopic {
 
 fun main() {
     println(Medium322().coinChange(intArrayOf(1, 2, 5), 11))
+    println(Medium322().coinChange(intArrayOf(2), 3))
+    println(Medium322().coinChange(intArrayOf(1), 0))
 }
