c41da117b8b939953caeb089b046abce5d9aa385
kkarpyshev <kkarpyshev@ivi.ru>
2022-03-02 19:14:37 +0300

    Easy118 challenge

--- src/easy/118. Pascal's Triangle .kt
+++ src/easy/118. Pascal's Triangle .kt
@@ -1,5 +1,8 @@
 package easy
 
+import ArraysTopic
+import DynamicProgrammingTopic
+
 /**
  * 118. Pascal's Triangle
  * https://leetcode.com/problems/pascals-triangle/
@@ -7,22 +10,22 @@ package easy
  * Given an integer numRows, return the first numRows of Pascal's triangle.
  */
 
-class Easy118 {
+class Easy118 : ArraysTopic, DynamicProgrammingTopic {
 
     fun generate(numRows: Int): List<List<Int>> {
         if (numRows == 0) return emptyList()
         val result = ArrayList<List<Int>>(numRows)
         result.add(listOf(1))
         for (i in 1 until numRows) {
-            val row = arrayOfNulls<Int>(i + 1)
+            val row = IntArray(i + 1)
             row[0] = 1
             row[i] = 1
-            for (j in 1..(i/2)) {
+            for (j in 1..(i / 2)) {
                 val t = result[i - 1][j - 1] + result[i - 1][j]
                 row[j] = t
                 row[i - j] = t
             }
-            result.add(i, row.filterNotNull())
+            result.add(i, row.toList())
         }
         return result
     }
